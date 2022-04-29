4cf40af2cdd258ecb43d0f60ef3a8c178f972185
Santiri2mil <santiago2mil@gmail.com>
2022-04-28 20:01:59 -0500

    2
    A

--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import { StyleSheet,Text, View } from "react-native";
+import { Button, TextInput } from "react-native-web";
 
 
 const StartGamesScreen =()=>{
-    return{
-        
-    }
+    return(
+        <View>
+            <Text>Select Number</Text>
+            <TextInput/>
+            <Button style={styles.button}/>
+            <Button style={styles.button}/>
+        </View>
+    )
 }
 
+const styles = StyleSheet.create({
+    header: {
+    }
 
+});
+
+export default StartGamesScreen
 
 
 
